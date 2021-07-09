a09da1b9fd4440b7cca7242fb696f5e51db1b5eb
7thedan <dannyan29@gmail.com>
2021-07-08 20:40:47 -0400

    Add project files.

---
+++ GetBuckets.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetBuckets.MVC.Startup))]
+namespace GetBuckets.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
